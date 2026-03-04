041d448048c4df42a195df3636fc5ebe6a0ca1c9
vibeofficial <ichioguchristopher98@gmail.com>
2026-03-04 18:07:13 +0100

    app use

--- brevo.js
+++ brevo.js
@@ -1,19 +1,22 @@
+require('dotenv').config();
 const Brevo = require('@getbrevo/brevo');
 
 
 exports.recieveEmail = async (options) => {
   try {
+    console.log("options:", options);
+
     const apikey = process.env.BREVO_API_KEY;
     const apiInstance = new Brevo.TransactionalEmailsApi();
     apiInstance.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, apikey);
     const sendSmtpEmail = new Brevo.SendSmtpEmail();
     sendSmtpEmail.subject = options.subject;
-    sendSmtpEmail.to = [{ email: options.myEmail }];
-    sendSmtpEmail.sender = { name: options.fullname, email: process.env.BREVO_SENDER_EMAIL };
+    sendSmtpEmail.to = [{ email: options.email }];
+    sendSmtpEmail.sender = { name: options.name, email: process.env.BREVO_SENDER_EMAIL };
     sendSmtpEmail.htmlContent = options.html;
     const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    console.log("Email sent to:", options.myEmail);
+    console.log("Email sent to:", options.email);
   } catch (error) {
-    throw new Error("Email not sent to:", options.myEmail)
+    throw new Error("Email not sent to:", options.email)
   }
 };
--- server.js
+++ server.js
@@ -11,39 +11,54 @@ app.use(cors());
 
 const router = express.Router();
 
-router.post('/email', (req, res) => {
-  try {
-    const { fullname, email, subject, message, myEmail } = req.body;
-    res.status(200).json({
-      status: true,
-      message: 'Message sent successfully'
-    });
+app.use(
+  router.post('/email', (req, res) => {
+    try {
+      const { fullname, email, subject, message, myEmail } = req.body;
+      const formattedName = `${fullname.split(' ')
+        .map(e => e[0]?.toUpperCase() + e.slice(1)?.toLowerCase())
+        .join(' ')
+        }`
+      res.status(200).json({
+        status: true,
+        message: 'Message sent successfully'
+      });
 
-    (async () => {
-      try {
-        await recieveEmail({
-          myEmail: myEmail.toLowerCase(),
-          fullname: `${fullname.split(' ').map(e => e[0].toUpperCase() + e.slice(1).toLowerCase()).join(' ')}`,
-          email: email.toLowerCase(),
-          subject: subject.toLowerCase(),
-          html: `
-                  <h2>New Contact Message from ${formattedName}</h2>
-                  <p><strong>Email:</strong> ${newContact.email}</p>
-                  <p><strong>User Type:</strong> ${role.toUpperCase()}</p>
-                  <p><strong>Message:</strong></p>
-                  <p>${message}</p>
-                `
-        });
-      } catch (error) {
-        console.error("Post-register async error:", error);
-      }
-    })();
-  } catch (error) {
-    res.status(500).json({
-      messae: `Error sending email: ${error.message}`
-    })
-  }
-});
+      (async () => {
+        try {
+          // await recieveEmail({
+          //   name: formattedName,
+          //   email: myEmail.toLowerCase(),
+          //   subject: subject,
+          //   html: `
+          //         <h2>New Contact Message from ${formattedName}</h2>
+          //         <p><strong>Email:</strong> ${email.toLowerCase()}</p>
+          //         <p><strong>Message:</strong></p>
+          //         <p>${message}</p>
+          //       `
+          // });
+       
+           const apikey = process.env.BREVO_API_KEY;
+              const apiInstance = new Brevo.TransactionalEmailsApi();
+              apiInstance.setApiKey(Brevo.TransactionalEmailsApiApiKeys.apiKey, apikey);
+              const sendSmtpEmail = new Brevo.SendSmtpEmail();
+              sendSmtpEmail.subject = options.subject;
+              sendSmtpEmail.to = [{ email: options.email }];
+              sendSmtpEmail.sender = { name: options.name, email: process.env.BREVO_SENDER_EMAIL };
+              sendSmtpEmail.htmlContent = options.html;
+              const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+              console.log("Email sent to:", options.email);
+            } catch (error) {
+              throw new Error("Email not sent to:", options.email)
+            }
+      })();
+    } catch (error) {
+      res.status(500).json({
+        messae: `Error sending email: ${error.message}`
+      })
+    }
+  })
+)
 
 app.listen(PORT, () => {
   console.log(`Server is listening to Port: ${PORT}`)
