6a5adcb26d20efba9f66d47e6c4b7107437a3d89
vibeofficial <ichioguchristopher98@gmail.com>
2026-03-08 12:21:34 +0100

    trim

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use('/email', async (req, res) => {
   try {
     const { fullname, clientEmail, subject, message, myEmail } = req.body;
     await recieveEmail({
-      name: `${fullname.split(' ').map(e => e[0]?.toUpperCase() + e.slice(1)?.toLowerCase())[0]}`,
+      name: `${fullname.trim().split(' ').map(e => e[0]?.toUpperCase() + e.slice(1)?.toLowerCase())[0]}`,
       email: myEmail.toLowerCase(),
-      subject: `${subject.split(' ').map(e => e[0]?.toUpperCase() + e.slice(1)?.toLowerCase()).join(' ')}`,
+      subject: `${subject.trim().split(' ').map(e => e[0]?.toUpperCase() + e.slice(1)?.toLowerCase()).join(' ')}`,
       html: `
               <h2>New Message from ${fullname.split(' ').map(e => e[0]?.toUpperCase() + e.slice(1)?.toLowerCase()).join(' ')}</h2>
               <p><strong>Email:</strong> ${clientEmail.toLowerCase()}</p>
-              <p>${message}</p>
+              <p>${message.trim()}</p>
             `
     });
     return res.status(200).json({
